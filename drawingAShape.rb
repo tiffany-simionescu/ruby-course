1170c4dd3764abcec179b893aec69632edaf55ea
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-14 00:12:43 -0400

    drawing a shape

---
+++ drawingAShape.rb
@@ -0,0 +1,17 @@
+# puts prints a new line after the string
+# print prints the string on the same line
+
+# Same Line
+print "Tiffany Simionescu"
+puts " is cool"
+
+# New Line
+puts "Tiffany Simionescu"
+print "can code in Ruby."
+
+# Shape
+puts ""
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
