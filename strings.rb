9e33422e8c3ddb4cd91c91959e30b80face4568d
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-15 01:27:52 -0400

    String Methods

---
+++ strings.rb
@@ -0,0 +1,16 @@
+puts "I love \"Ruby\"."
+puts "Dougie\nSophie"
+
+phrase = " My name is Tiffany. "
+# adding a string method
+puts phrase.upcase()
+puts phrase.downcase()
+# strips away white space
+puts phrase.strip()
+puts phrase.length()
+puts phrase.include? "Tiffany"
+puts phrase[1]
+puts phrase[12, 7]
+puts phrase.index("Tiffany")
+
+puts "programming".upcase()
