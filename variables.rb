53f18c33c3c0f70c9c335e17c037517e02144006
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-15 00:55:53 -0400

    Variables

---
+++ variables.rb
@@ -0,0 +1,17 @@
+character_name = "Tiffany"
+character_age = "33"
+
+puts ("My name is " + character_name + ".")
+puts ("I am " + character_age + " years-old.")
+
+character_name = "Dougie"
+character_age = "8"
+
+puts ("My son's name is " + character_name + ".")
+puts ("He is " + character_age + " years-old.")
+
+character_name = "Sophie"
+character_age = "4"
+
+puts ("My daughter's name is " + character_name + ".")
+puts ("She is " + character_age + " years-old.")
