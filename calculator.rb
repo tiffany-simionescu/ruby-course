06d48c3aacfbbaa57b4c470ba04885d2c15844f2
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-20 01:45:28 -0400

    Added calculator.

---
+++ calculator.rb
@@ -0,0 +1,7 @@
+puts "Enter a number: "
+num1 = gets.chomp().to_f
+puts "Enter another number: "
+num2 = gets.chomp().to_f
+# convert strings to integers with to_i
+# convert string to floats with to_f
+puts (num1 + num2)
