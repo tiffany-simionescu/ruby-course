8fa794d65c4b0325a1f1faccdc11acd2e1ee3ffc
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-20 01:27:27 -0400

    Added numbers.

---
+++ numbers.rb
@@ -0,0 +1,37 @@
+puts 5
+puts 5.8423
+puts -54.32
+puts 5 + 9
+# + - * /
+
+# power
+puts 2**3
+
+# Modulus remainder
+puts 10 % 3
+
+num = 25
+# must change number to string with to_s method
+puts ("My favorite num is " + num.to_s)
+
+num = 20.687
+puts num.round()
+
+num = 20.1
+puts num.ceil()
+
+num = 20.9
+puts num.floor()
+
+# absolute value
+num -20 = abs()
+
+num 20.9
+puts Math.sqrt(num)
+
+puts 1 + 7
+puts 1.0 + 7
+
+puts 10 / 7
+puts 10 / 7.0
+
