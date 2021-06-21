0242813cc1f62022bceefdf3b127eab83a7c634a
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-21 18:36:43 -0400

    Added case expressions

---
+++ case_expressions.rb
@@ -0,0 +1,28 @@
+def get_day_name(day)
+  day_name = ""
+
+  case day.downcase
+  when "mon"
+    day_name = "Monday"
+  when "tue"
+    day_name = "Tuesday"
+  when "wed"
+    day_name = "Wednesday"
+  when "thu"
+    day_name = "Thursday"
+  when "fri"
+    day_name = "Friday"
+  when "sat"
+    day_name = "Saturday"
+  when "sun"
+    day_name = "Sunday"
+  else
+    day_name = "Invalid abbreviation"
+  end 
+
+  return day_name
+end
+
+puts get_day_name("Mon")
+puts get_day_name("fri")
+puts get_day_name("123")
