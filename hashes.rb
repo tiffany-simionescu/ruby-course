00cb92e913c2e0bd7d85f91c2a62bd761d6e5ae3
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-21 00:16:00 -0400

    Added hashes

---
+++ hashes.rb
@@ -0,0 +1,13 @@
+states = {
+  # key => value
+  # Keys are unique
+  "Pennsylvania" => "PA",
+  "New York" => "NY",
+  "Oregon" => "OR",
+  :Florida => "FL",
+  1 => "SC"
+}
+
+puts states["Oregon"]
+puts states[:Florida]
+puts states[1]
