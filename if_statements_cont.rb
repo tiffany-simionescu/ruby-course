5d31c48b197477ee4236f41e46b42854970e388a
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-21 15:11:31 -0400

    Added if statements cont

---
+++ if_statements_cont.rb
@@ -0,0 +1,13 @@
+def max(num1, num2, num3)
+  if num1 >= num2 and num1 >= num3
+    return num1
+  elsif num2 >= num1 and num2 >= num3
+    return num2
+  else
+    return num3
+  end
+end
+
+puts max(1, 2, 3)
+puts max(3, 2, 1)
+puts max(1, 3, 2)
