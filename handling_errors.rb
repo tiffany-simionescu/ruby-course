55c26490bb734646d21a2696d0dd64b5ae289406
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-22 14:20:50 -0400

    Updated handling errors.

--- handling_errors.rb
+++ handling_errors.rb
@@ -7,9 +7,9 @@ lucky_nums = [4, 8, 15, 16, 23, 42]
 
 begin
   lucky_nums["dog"]
-  # num = 10 / 0
+  num = 10 / 0
 rescue ZeroDivisionError => e
-  puts ("Division by zero error: " + e)
+  puts (e)
 rescue TypeError => e
-  puts ("Type error: " + e)
+  puts (e)
 end
