d99c87228306f56bc96418ff2e204551ae50eb31
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-22 14:30:37 -0400

    Added classes and objects.

---
+++ classes_and_objects.rb
@@ -0,0 +1,13 @@
+class Book
+  attr_accessor :title, :author, :pages 
+end
+
+book1 = Book.new()
+book1.title = "Harry Potter"
+book1.author = "JK Rowling"
+book1.pages = 400
+  
+puts book1.title
+puts book1.author 
+puts book1.pages
+puts book1.inspect
