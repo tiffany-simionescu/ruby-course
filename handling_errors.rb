c069b0cfe922adee1bb75df85fc33f957035fb13
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-22 14:02:05 -0400

    Added handling errors.

---
+++ handling_errors.rb
@@ -0,0 +1,15 @@
+# can't divide a number by 0
+# num = 10 / 0
+
+# type error
+lucky_nums = [4, 8, 15, 16, 23, 42]
+# lucky_nums["dog"]
+
+begin
+  lucky_nums["dog"]
+  # num = 10 / 0
+rescue ZeroDivisionError => e
+  puts ("Division by zero error: " + e)
+rescue TypeError => e
+  puts ("Type error: " + e)
+end
