cea12e94133622ccf669cb11a0a9c0b723d9b765
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-21 20:13:10 -0400

    Added for loops

---
+++ for_loops.rb
@@ -0,0 +1,17 @@
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+for friend in friends
+  puts friend
+end
+
+friends.each do |friend|
+  puts friend
+end
+
+for index in 0..5
+  puts index
+end
+
+6.times do |index|
+  puts index
+end
