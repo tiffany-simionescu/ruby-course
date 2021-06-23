ebd68a73f30d59db84d4c624601cd8890e767380
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-22 21:26:10 -0400

    Added irb notes

---
+++ irb.rb
@@ -0,0 +1,11 @@
+# Interactive Ruby = irb
+# a little application that we can run from the terminal
+# or the command prompt.
+# Allows us to use and test out the different ruby commands
+# in a sort of sandbox environment
+
+# open terminal
+# irb -v
+
+# type irb in the terminal to start irb
+# Not a replacement for a file
